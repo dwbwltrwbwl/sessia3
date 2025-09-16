ba1d10e3b91a1b17d10e29522d5f19e425e28863
10220448 <10220448@D205PC25>
2025-09-16 11:04:35 +0300

    Добавьте файлы проекта.

---
+++ sessia3/ApplicationData/AppConnect.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sessia3.ApplicationData
+{
+    internal class AppConnect
+    {
+        public static sessia3Entities model01;
+    }
+}
